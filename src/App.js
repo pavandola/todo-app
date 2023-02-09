54cc821e7c44ba227c441868754e316271ccd912
pavan dola <pavandola97@gmail.com>
2023-02-09 12:43:39 +0530

    s

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Form from 'react-bootstrap/Form';
+import TodoList from "./TodoList";
+
+
+const App = () => {
+  const [task,setTask] = useState("");
+  const[todos,setTodos]= useState([]);
+  const changeHandler = e =>{
+   setTask(e.target.value)
+  }
+  const submitHandler = e =>{
+    e.preventDefault();
+    const newTodos = [...todos,task];
+    setTodos(newTodos)
+  }
+  const deleteHandler = (indexValue) =>{
+    const newTodos = todos.filter((todo,index) => index !== indexValue);
+    setTodos(newTodos);
+  }
+
+  return(
+   <div>
+    <center>
+      <div className="card">
+        <div className="card-body">
+            <h5 className="card-title">Todo Mangement Application</h5>
+            <form onSubmit={submitHandler}>
+              <input type="text" name="task" value={task} onChange={changeHandler}/> 
+              <input type="submit" value="Add" name="Add" />
+
+            </form>
+            <TodoList todolist={todos} deleteHandler={deleteHandler}/>
+        </div>
+      </div>
+    </center>
+   </div>
+
+  )
+  
+}
+export default App;
